18ce1605fab130a441ae29bd7d68517ec5ee381b
Broderick Young <bxyoung89@gmail.com>
2015-10-06 23:09:46 -0500

    Fixed dependency that is breaking.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'jade');
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
